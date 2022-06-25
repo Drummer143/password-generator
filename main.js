01e4d4cf6312b877fb8335f641c30ee2631bd5dd
Drummer143 <berg.evg.1@gmail.com>
2022-06-25 16:15:40 +0300

    feat: made adaptive design

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ const handleChange = (min, max, id) => {
 
 function generatePasswords(useCapitals, useNumbers, useSymbols, count, length) {
     const charset = [
-        /* smalls: */ "abcdefghijklmnopqrstuvwxyz",
-        /* capitals: */ "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        /* numbers: */[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-        /* symbols: */[33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126]
+        "abcdefghijklmnopqrstuvwxyz",
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126]
     ]
 
     const allowedSymbols = [useCapitals, useNumbers, useSymbols];
@@ -42,10 +42,7 @@ function generatePasswords(useCapitals, useNumbers, useSymbols, count, length) {
 
     let div = document.createElement('div');
     div.className = 'result';
-    div.innerHTML = '';
-    div.style.gridTemplateColumns = `repeat(${Math.round(32 / length)}, 1fr)`;
 
-    let passwords = [];
     const rand = () => Math.round(Math.random() * 10000) + 1;
 
     for (let i = 0; i < count; i++) {
@@ -62,24 +59,16 @@ function generatePasswords(useCapitals, useNumbers, useSymbols, count, length) {
             }
         }
 
-        const result = document.createElement('h2');
-        result.className = 'password';
-        result.append(password);
-        console.log(result);
+        const result = document.createElement('div');
+        result.className = 'cell';
+        result.innerHTML = '<h2 class="password"></h2>'
+        result.childNodes[0].append(password);
 
         div.append(result);
     }
 
-    console.log(div);
-
-    /* let results = document.createElement=('div'); */
-
     document.getElementById('root').innerHTML = '';
     document.getElementById('root').append(div);
-
-    // console.log(Math.round(Math.random() * 10000) % 4);
-
-    // alert(`${charset[0].length} ${charset[1].length} ${charset[2].length} ${charset[3].length}`)
 }
 
 document.getElementById('capitals').addEventListener('click', e => useCapitals = handleClick(e.target.checked, 'capitals'));
@@ -88,13 +77,5 @@ document.getElementById('symbols').addEventListener('click', e => useSymbols = h
 
 document.getElementById('count').onchange = () => count = handleChange(1, 100, 'count');
 document.getElementById('length').onchange = () => length = handleChange(8, 32, 'length');
-
-document.getElementById('button').onclick = e => alert(
-    `Capital: ${useCapitals}
-Numbers: ${useNumbers}
-Symbols: ${useSymbols}
-Count of passwords: ${count}
-Length: ${length}`
-);
 
 document.getElementById('button').onclick = () => generatePasswords(useCapitals, useNumbers, useSymbols, count, length);
